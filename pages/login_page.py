a5b63507eb8ce373f0e64a11b3ae43f742959643
Артем Огурцов <ogurec98@yandex.ru>
2022-10-04 15:50:45 +0300

    project third step fix

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from lib2to3.pgen2 import driver
+from .base_page import BasePage
+from .locator import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', 'Invalid URL'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "No 'Log In' form was found"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "No 'Register' form was found"
